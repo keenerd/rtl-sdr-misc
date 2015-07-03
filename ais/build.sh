d2c44ed9560bae42a73c74de468c821415e7a1ce
dgiardini <dgiardini@gmail.com>
2015-07-03 12:45:17 -0300

    compile aisdecoder files too

--- ais/build.sh
+++ ais/build.sh
@@ -1,12 +1,28 @@
 #!/bin/sh
-
 # todo, a real makefile
 
-files="rtl_ais.c convenience.c"
-flags="-Wall -O2"
-includes="-I/usr/include/libusb-1.0"
-libs="-lusb-1.0 -lrtlsdr -lpthread -lm"
+#point this to the correct path
+RTLSDR_PATH="../../src"
+
+files="$RTLSDR_PATH/rtl_ais.c $RTLSDR_PATH/convenience/convenience.c \
+		$RTLSDR_PATH/aisdecoder/aisdecoder.c $RTLSDR_PATH/aisdecoder/sounddecoder.c \
+		$RTLSDR_PATH/aisdecoder/lib/receiver.c 
+		$RTLSDR_PATH/aisdecoder/lib/protodec.c
+		$RTLSDR_PATH/aisdecoder/lib/hmalloc.c
+		$RTLSDR_PATH/aisdecoder/lib/filter.c "
+		
+flags="-Wall -O2 "
+includes="-I/usr/include/libusb-1.0 -I../../include -I ../../src/convenience -I ../../src/aisdecoder -I ../../src/aisdecoder/lib"
+libs="-L/usr/lib -L. -lusb-1.0 -lrtlsdr -lpthread -lm "
+
+UNAME=$(uname)
+if [ "$UNAME" != "Linux" ]
+then
+# Conditional section for Windows
+	libs="$libs -lWs2_32"
+fi
 
 rm -f rtl_ais
-gcc -o rtl_ais $files $flags $includes $libs
+echo gcc -o rtl_ais $files $flags $includes $libs 
+gcc -o rtl_ais $files $flags $includes $libs 
 
