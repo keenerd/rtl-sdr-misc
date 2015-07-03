0014a8392eafba5963e7bffb06bf0026b7f165e4
dgiardini <dgiardini@gmail.com>
2015-07-03 13:16:36 -0300

    build script

--- ais/build.sh
+++ ais/build.sh
@@ -2,17 +2,17 @@
 # todo, a real makefile
 
 #point this to the correct path
-RTLSDR_PATH="../../src"
+RTLSDR_PATH="/tmp/rtl-sdr-exp/src"
 
-files="$RTLSDR_PATH/rtl_ais.c $RTLSDR_PATH/convenience/convenience.c \
-		$RTLSDR_PATH/aisdecoder/aisdecoder.c $RTLSDR_PATH/aisdecoder/sounddecoder.c \
-		$RTLSDR_PATH/aisdecoder/lib/receiver.c 
-		$RTLSDR_PATH/aisdecoder/lib/protodec.c
-		$RTLSDR_PATH/aisdecoder/lib/hmalloc.c
-		$RTLSDR_PATH/aisdecoder/lib/filter.c "
+files="rtl_ais.c convenience.c \
+		./aisdecoder/aisdecoder.c ./aisdecoder/sounddecoder.c \
+		./aisdecoder/lib/receiver.c 
+		./aisdecoder/lib/protodec.c
+		./aisdecoder/lib/hmalloc.c
+		./aisdecoder/lib/filter.c "
 		
 flags="-Wall -O2 "
-includes="-I/usr/include/libusb-1.0 -I../../include -I ../../src/convenience -I ../../src/aisdecoder -I ../../src/aisdecoder/lib"
+includes="-I/usr/include/libusb-1.0 -I./aisdecoder -I ./aisdecoder/lib"
 libs="-L/usr/lib -L. -lusb-1.0 -lrtlsdr -lpthread -lm "
 
 UNAME=$(uname)
